b627169a905c8b1820f41c4204d9dc2607990016
duynvph15542 <duynvph15542@fpt.edu.vn>
2022-06-02 06:24:00 +0700

    edit-product

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -6,13 +6,22 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductsService {
-API_URL:string ='http://localhost:3000/products'
+  API_URL: string = 'http://localhost:3000/products'
 
-  constructor(private http:HttpClient) { }
-  addProduct(product:any):Observable<Iproduct>{
-    return this.http.post<Iproduct>(this.API_URL,product)
+  constructor(private http: HttpClient) { }
+  addProduct(product: any): Observable<Iproduct> {
+    return this.http.post<Iproduct>(this.API_URL, product)
   }
-  getProducts():Observable<Iproduct[]>{
+  getProducts(): Observable<Iproduct[]> {
     return this.http.get<Iproduct[]>(this.API_URL);
+  }
+  getProduct(id: string | number): Observable<Iproduct> {
+    return this.http.get<Iproduct>(`${this.API_URL}/${id}`)
+  }
+  removeProduct(id: number): Observable<Iproduct> {
+    return this.http.delete<Iproduct>(`${this.API_URL}/${id}`)
+  }
+  updateProduct(product: Iproduct): Observable<Iproduct> {
+    return this.http.put<Iproduct>(`${this.API_URL}/${product.id}`, product);
   }
 }
