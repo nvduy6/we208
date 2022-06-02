ba84443dbba8336d5306f17a83e149b6ea057a8e
duynvph15542 <duynvph15542@fpt.edu.vn>
2022-06-02 09:30:48 +0700

    list_cate

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IPcate } from 'src/app/models/Category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
+categoryList!:IPcate[];
+  constructor(
+    private categoryService:CategoryService
+  ) { }
 
   ngOnInit(): void {
+  }
+  ShowCategory(){
+    this.categoryService.getCates().subscribe(data=>{
+      this.categoryList=data;
+    })
+  }
+  onRemoveItem(id:number){
+    const confirm = window.confirm('Ban co muon xoa khong');
+    if(confirm){
+      this.categoryService.removeCate(id).subscribe(()=>{
+        this.categoryList=this.categoryList.filter(item=>item.id!==id)
+      })
+    }
   }
 
 }
